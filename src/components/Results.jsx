23152c4babc72572a1c56f161a2a2d48417ec03f
Mehmet <mathdenizi@gmail.com>
2024-01-30 14:06:52 +0100

    Computing values

---
+++ src/components/Results.jsx
@@ -0,0 +1,28 @@
+import { calculateInvestmentResults } from "../util/investment.js";
+
+export default function Results({ input }) {
+  const resultsData = calculateInvestmentResults(input);
+
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment Value</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultsData.map((yearData) => {
+          return (
+            <tr key={yearData.year}>
+              <td></td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
